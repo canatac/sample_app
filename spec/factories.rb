4699bb0ede3174709db4c54eabbf129311cf058c
Can ATAC <can.atac@gmail.com>
2014-04-09 07:20:23 +0200

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+    factory :user do
+    
+    name "Can ATAC"
+    email "can.atac@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+    
+    end
+end
